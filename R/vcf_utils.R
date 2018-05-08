503d3b0219bdeb03fe541a3ed52ac64028d7c7aa
Martin Petr <bodkan@users.noreply.github.com>
2018-05-08 15:57:12 +0200

    Skip the filtering step if it's not necessary

--- R/vcf_utils.R
+++ R/vcf_utils.R
@@ -2,14 +2,18 @@
 filter_muts <- function(vcf, mut_type = NULL, pop_origin = NULL, t_min = -Inf, t_max = Inf) {
   vcf_info <- VariantAnnotation::info(vcf)
 
-  mut_pos <- vcf_info$GO >= t_min & vcf_info$GO <= t_max
+  if (is.null(mut_type) & is.null(pop_origin) & t_min == -Inf & t_max == Inf)
+      mut_pos <- rep(TRUE, nrow(vcf_info))
+  } else {
+    mut_pos <- vcf_info$GO >= t_min & vcf_info$GO <= t_max
 
-  if (!is.null(mut_type)) {
-    mut_pos <- mut_pos & vcf_info$MT == mut_type
-  }
+    if (!is.null(mut_type)) {
+      mut_pos <- mut_pos & vcf_info$MT == mut_type
+    }
 
-  if (!is.null(pop_origin)) {
-    mut_pos <- mut_pos & (vcf_info$PO == pop_origin)
+    if (!is.null(pop_origin)) {
+      mut_pos <- mut_pos & (vcf_info$PO == pop_origin)
+    }
   }
 
   mut_pos
